6d12520ddc299670cae7c9fdedb6263d1ec899b3
JCRodriguez93 <jorge_madro@hotmail.com>
2026-03-23 21:22:57 +0100

    feat: entornos virtuales y dependencias iniciales

---
+++ web/check.py
@@ -0,0 +1,7 @@
+# web/check.py
+import sys
+print('=' * 50)
+print('Verificación del entorno Flask')
+print('=' * 50)
+print(f'Python versión: {sys.version}')
+print(f'Intérprete: {sys.executable}')
