d3cd3843aa1c56ad935fe56fa989ea4e1e4b4336
JCRodriguez93 <jorge_madro@hotmail.com>
2026-04-21 13:08:47 +0200

    feat: estructura Flask con Blueprints, Application Factory y base.html

---
+++ web/app/routes/tasks.py
@@ -0,0 +1,7 @@
+
+# web/app/routes/tasks.py
+from flask import Blueprint, render_template
+tasks = Blueprint('tasks', __name__)
+@tasks.route('/mis-tareas')
+def mis_tareas():
+    return render_template('projects/lista.html', proyectos=[], tareas=[])
